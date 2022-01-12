59884cb4d5f67a9eb0a19a469ffe31959c32d04d
David Molineus <david.molineus@netzmacht.de>
2022-01-12 16:31:10 +0100

    Use json output of composer-require-checker
    Require ^3.8 of composer-require-checker which introduces the json output and increase minimum PHP version to 7.4 as composer-require-checker requires it

--- tests/ComposerRequireCheckerPluginTest.php
+++ tests/ComposerRequireCheckerPluginTest.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace Phpcq\ComposerRequireCheckerPluginTest;
 
+use Generator;
 use Phpcq\PluginApi\Version10\Configuration\PluginConfigurationBuilderInterface;
 use Phpcq\PluginApi\Version10\Configuration\PluginConfigurationInterface;
 use Phpcq\PluginApi\Version10\DiagnosticsPluginInterface;
 use Phpcq\PluginApi\Version10\EnvironmentInterface;
+use Phpcq\PluginApi\Version10\Output\OutputInterface;
+use Phpcq\PluginApi\Version10\Output\OutputTransformerFactoryInterface;
+use Phpcq\PluginApi\Version10\Report\TaskReportInterface;
+use Phpcq\PluginApi\Version10\Task\PhpTaskBuilderInterface;
+use Phpcq\PluginApi\Version10\Task\ReportWritingTaskInterface;
+use Phpcq\PluginApi\Version10\Task\TaskFactoryInterface;
 use PHPUnit\Framework\TestCase;
+
+use function iterator_to_array;
+use function json_encode;
 
 /**
  * @coversNothing
@@ -44,5 +54,114 @@ final class ComposerRequireCheckerPluginTest extends TestCase
 
         // We assume it worked out as the plugin did execute correctly.
         $this->addToAssertionCount(1);
+    }
+
+    public function provideTestReportData(): Generator
+    {
+        yield 'Broken output' => [
+            'diagnostics' => [
+                [
+                    'severity' => TaskReportInterface::SEVERITY_FATAL,
+                    'message' => 'Unable to parse output: Syntax error'
+                ]
+            ],
+            'output' => 'foo bar'
+        ];
+
+        yield 'No unknown symbols found' => [
+            'diagnostics' => [
+                [
+                    'severity' => TaskReportInterface::SEVERITY_INFO,
+                    'message' => 'There were no unknown symbols found.'
+                ]
+            ],
+            'output' => json_encode(['unknown-symbols' => []])
+        ];
+
+        yield 'Unknown symbols detected' => [
+            'diagnostics' => [
+                [
+                    'severity' => TaskReportInterface::SEVERITY_MAJOR,
+                    'message' => 'Missing dependency "ext-json" (used symbols: "json_encode", "json_decode")'
+                ],
+                [
+                    'severity' => TaskReportInterface::SEVERITY_MAJOR,
+                    'message' => 'Missing dependency "vendor/foo" (used symbols: "Foo\Bar")'
+                ],
+                [
+                    'severity' => TaskReportInterface::SEVERITY_MAJOR,
+                    'message' => 'Missing dependency "vendor/bar" (used symbols: "Foo\Bar")'
+                ]
+            ],
+            'output' => json_encode(
+                [
+                    'unknown-symbols' => [
+                        'json_encode' => ['ext-json'],
+                        'json_decode' => ['ext-json'],
+                        'Foo\Bar' => ['vendor/foo', 'vendor/bar']
+                    ]
+                ]
+            )
+        ];
+    }
+
+    /** @dataProvider provideTestReportData */
+    public function testReport(array $diagnostics, string $output): void
+    {
+        $config = $this->getMockForAbstractClass(PluginConfigurationInterface::class);
+        $environment = $this->getMockForAbstractClass(EnvironmentInterface::class);
+        $transformerFactory = null;
+        $task = $this->getMockForAbstractClass(ReportWritingTaskInterface::class);
+
+        $taskBuilder = $this->getMockForAbstractClass(PhpTaskBuilderInterface::class);
+        $taskBuilder
+            ->expects($this->once())
+            ->method('build')
+            ->withAnyParameters()
+            ->willReturn($task);
+
+        $taskBuilder
+            ->expects($this->once())
+            ->method('withWorkingDirectory')
+            ->withAnyParameters()
+            ->willReturn($taskBuilder);
+
+        $taskBuilder
+            ->expects($this->once())
+            ->method('withOutputTransformer')
+            ->withAnyParameters()
+            ->willReturnCallback(
+                function (OutputTransformerFactoryInterface $factory) use (&$transformerFactory, $taskBuilder) {
+                    $transformerFactory = $factory;
+
+                    return $taskBuilder;
+                }
+            );
+
+        $taskFactory = $this->getMockForAbstractClass(TaskFactoryInterface::class);
+        $taskFactory
+            ->expects($this->once())
+            ->method('buildRunPhar')
+            ->withAnyParameters()
+            ->willReturn($taskBuilder);
+
+        $environment
+            ->expects($this->once())
+            ->method('getTaskFactory')
+            ->willReturn($taskFactory);
+
+        iterator_to_array($this->instantiate()->createDiagnosticTasks($config, $environment));
+
+        self::assertInstanceOf(OutputTransformerFactoryInterface::class, $transformerFactory);
+
+        $report = $this->getMockForAbstractClass(TaskReportInterface::class);
+        $report
+            ->expects($this->exactly(count($diagnostics)))
+            ->method('addDiagnostic')
+            ->withConsecutive(... $diagnostics);
+
+        $transformer = $transformerFactory->createFor($report);
+        $transformer->write($output, OutputInterface::CHANNEL_STDOUT);
+        $transformer->finish(0);
     }
 }
